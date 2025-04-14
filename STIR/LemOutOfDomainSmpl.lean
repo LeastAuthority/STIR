553d6e495dae92e2ec20990e3159bc657055410d
PlanetMacro <mirco@leastauthority.com>
2025-04-14 13:34:51 +0200

    out of bound lemma 1

--- STIR/LemOutOfDomainSmpl.lean
+++ STIR/LemOutOfDomainSmpl.lean
@@ -17,12 +17,23 @@ noncomputable def listDecodingCollisionProbability
       u ≠ u' ∧ u ∈ C.List u δ ∧ u' ∈ C.List u δ ∧
       ∀ i : Fin s, (r i).val  = (r i).val} -- TODO: We need the RSC polynomial
 
-      -- ∀ i : Fin s, Polynomial.eval (r i).val u = Polynomial.eval (r i).val u'
+      -- TODO: ∀ i : Fin s, Polynomial.eval (r i).val u = Polynomial.eval (r i).val u'
+      ---         requires to get the assiciated polynomial of a ReedSolomon code
 
-lemma randomSampleCollisionBound
+lemma randomSampleCollisionBound_1
   {F : Type*} [Field F] [Fintype F] [DecidableEq F]
   (C : ReedSolomonCode F)
   (δ : ℝ)
   (l s : ℕ)
   (h_decodable : C.listDecodable δ l) :
-  listDecodingCollisionProbability C δ s C.domainComplementNonempty ≤ 1 := by sorry
+  listDecodingCollisionProbability C δ s C.domainComplementNonempty ≤
+    (l.choose 2) * ((C.d - 1) / (Fintype.card F - C.L.card))^s := by sorry
+
+lemma randomSampleCollisionBound_2
+  {F : Type*} [Field F] [Fintype F] [DecidableEq F]
+  (C : ReedSolomonCode F)
+  (δ : ℝ)
+  (l s : ℕ)
+  (h_decodable : C.listDecodable δ l) :
+  listDecodingCollisionProbability C δ s C.domainComplementNonempty ≤
+    (l^2 / 2) * (C.d / (Fintype.card F - C.L.card))^s := by sorry
