f487d25c72f2e0a50850c2f4fedfc7d2fa995830
PlanetMacro <mirco@leastauthority.com>
2025-04-17 12:48:25 +0200

    RSC poly

--- STIR/DefReedSolomonCodes.lean
+++ STIR/DefReedSolomonCodes.lean
@@ -4,16 +4,15 @@ import Mathlib.FieldTheory.Finite.Basic
 import Mathlib.FieldTheory.Finite.GaloisField
 import Mathlib.Algebra.Polynomial.Basic
 import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Finset.Card
 import Mathlib.Tactic.NormNum
 import Mathlib.Data.Nat.Prime.Basic
+import Mathlib.LinearAlgebra.Lagrange
 
 /-TODO: Make C.d a positive natural number, because expressions like (1-C.d) occure
   e.g. in the OutOfDomainSmpl lemma and for d=0 this makes no sense-/
 
-/- THIS IS A PLACEHOLDER STRUCTURE AND NOT A GOOD DEFINITION YET. WE NEED THE SET OF
-   CODEWORDS TO BE FINITE AND IN ORDER FOR FINSET TO BE CONSTRUCTED WE NEED TO EXPLICITLY
-   COMPUTE AN ASSOCIATED POLYNOMIAL TO ANY GIVEN CODEWORD [E.G. BY LAGRANGE INTERPOLATION]
-   TBD -/
+
 
 /-- Polynomials of degree < d over a finite field F -/
 noncomputable def polynomialsUpTo (F : Type*) [Field F] [Fintype F] [DecidableEq F] (d : ℕ)
@@ -38,6 +37,8 @@ structure ReedSolomonCode
     nonempty_L : L.Nonempty
     code       : Finset (L → F)
     code_def   : code = (polynomialsUpTo F d).image (polynomialEvalOn F L)
+
+
 
 namespace ReedSolomonCode
 
@@ -64,5 +65,15 @@ noncomputable def domainComplement (_C : ReedSolomonCode F L d) : Finset F :=
   Finset.univ \ L
 
 lemma domainComplementNonempty (C : ReedSolomonCode F L d) : Nonempty C.domainComplement := by sorry
+
+/-- Restrict `f : L → F` to a subset `S ⊆ L`. -/
+def restrictTo (f : L → F) (S : Finset F) (hS : S ⊆ L) : (↑S → F) :=
+  fun x => f ⟨x, hS x.2⟩
+
+noncomputable def poly
+  (C : ReedSolomonCode F L d)
+  (f : C.code) : Polynomial F :=
+    Lagrange.interpolate L.attach (fun i => (i : F)) f
+
 
 end ReedSolomonCode
