b4f9ed2340ac7dc0167d1bc5e3b63213b510832d
PlanetMacro <mirco@leastauthority.com>
2025-04-23 13:02:44 +0200

    folding final

--- STIR/DefProximityBound.lean
+++ STIR/DefProximityBound.lean
@@ -5,26 +5,19 @@ import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Real.Basic
 import Mathlib.Data.Real.Sqrt
 
-  noncomputable def Bstar
-  {F : Type*} [Field F] [Fintype F] [DecidableEq F]
-  {L : Finset F}
-  {d : ℕ}
-  (C : ReedSolomonCode F L d) : ℝ :=
-  Real.sqrt C.rate
+noncomputable def Bstar
+  (x: ℝ) : ℝ := Real.sqrt x
 
-/- TODO: Make Everything ENNReal in the first place. Working with probabilities ENNReal seems to be the proper type. We might want to discuss this with the larger team at some point -/
 noncomputable def err'
-  {F : Type*} [Field F] [Fintype F] [DecidableEq F]
-  {L : Finset F}
-  {d : ℕ}
-  (C : ReedSolomonCode F L d)
-  (Rnge : ℝ)
-  (δ : {x : ℝ // 0 < x ∧ x < Rnge})
-  (m : ℕ) : ENNReal :=
-  ENNReal.ofReal (
-    if δ.val ≤ (1 - C.rate) / 2 then
-      ((m - 1 : ℝ) * d) / (C.rate * (Fintype.card F))
+  (F : Type*) [Fintype F]
+  (d : ℕ)
+  (ρ : ℝ)
+  (δ : ℝ)
+  (m: ℕ) : ENNReal :=
+    ENNReal.ofReal (
+    if δ ≤ (1 - ρ) / 2 then
+      ((m - 1) * d) / (ρ * (Fintype.card F))
     else
-      let min_val := min (Rnge - δ.val) (C.rate / 20)
-      ((m - 1 : ℝ) * (d : ℝ)^2) / ((Fintype.card F) * (2 * min_val)^7)
+      let min_val := min (1 - (Real.sqrt ρ) - δ ) ((Real.sqrt ρ) / 20)
+      ((m - 1) * d^2) / ((Fintype.card F) * (2 * min_val)^7)
   )
