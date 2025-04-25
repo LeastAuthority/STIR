48610f45d5c18d494ff19b8f9535bb51e95368bb
PlanetMacro <mirco@leastauthority.com>
2025-04-25 11:34:00 +0200

    Comments&audit of definitions

--- STIR/DefFracHammingDist.lean
+++ STIR/DefFracHammingDist.lean
@@ -8,13 +8,12 @@ import Mathlib.FieldTheory.Finite.Basic
 import Mathlib.Data.Real.Basic
 import Mathlib.InformationTheory.Hamming
 
-/-- The fractional Hamming distance between two functions `f, g : ↑L → F` is defined as ∆(f, g) := (number of positions at which f and g differ) / |L| -/
+/-- The fractional Hamming distance between two functions f, g : L → F is defined as ∆(f, g) := (number of positions at which f and g differ) / |L| -/
 def fractionalHammingDist {F : Type*} {L : Finset F} [DecidableEq F]
-  (f g : ↑L → F) : ℚ := (hammingDist f g : ℚ) / Fintype.card (↑L)
+  (f g : ↑L → F) : ℚ := (hammingDist f g : ℚ) / Fintype.card L
 
-/-- The fractional Hamming distance between a function `f : ↑L → F` and a nonempty finite set of functions `S ⊆ (↑L → F)`
-    is defined as ∆(f, S) := minₕ∈S ∆(f, h) -/
+/-- The fractional Hamming distance between a function f : L → F and a nonempty finite set of functions S ⊆ (L → F) is defined as ∆(f, S) := min_{h ∈ S} ∆(f, h) -/
 def fractionalHammingDistSet {F : Type*} {L : Finset F} [DecidableEq F]
   (f : ↑L → F)
   (S : Finset (↑L → F))
-  (h_nonempty : S.Nonempty) : ℚ := (S.inf' h_nonempty (hammingDist f ·) : ℚ) / Fintype.card (↑L)
+  (h_nonempty : S.Nonempty) : ℚ := (S.inf' h_nonempty (hammingDist f ·) : ℚ) / Fintype.card L
--- STIR/DefProximityBound.lean
+++ STIR/DefProximityBound.lean
@@ -4,26 +4,21 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Least Authority
 -/
 
-import STIR.DefReedSolomonCodes
-
 import Mathlib.FieldTheory.Finite.Basic
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Real.Basic
 import Mathlib.Data.Real.Sqrt
 
-noncomputable def Bstar
-  (x: ℝ) : ℝ := Real.sqrt x
+/-- Proximity bound function -/
+noncomputable def Bstar (x: ℝ) : ℝ := Real.sqrt x
 
-noncomputable def err'
-  (F : Type*) [Fintype F]
-  (d : ℕ)
-  (ρ : ℝ)
-  (δ : ℝ)
-  (m: ℕ) : ENNReal :=
-    ENNReal.ofReal (
-    if δ ≤ (1 - ρ) / 2 then
-      ((m - 1) * d) / (ρ * (Fintype.card F))
-    else
-      let min_val := min (1 - (Real.sqrt ρ) - δ ) ((Real.sqrt ρ) / 20)
-      ((m - 1) * d^2) / ((Fintype.card F) * (2 * min_val)^7)
-  )
+/-- Proximity error function -/
+noncomputable def err' (F : Type*) [Fintype F] (d : ℕ) (ρ : ℝ) (δ : ℝ) (m: ℕ) : ENNReal :=
+  -- coerced to ENNReal, because it is exclusively used in probability inequalities and we agreed to coerce ℝ → ENNReal not vice versa
+  ENNReal.ofReal (
+  if δ ≤ (1 - ρ) / 2 then
+    ((m - 1) * d) / (ρ * (Fintype.card F))
+  else
+    let min_val := min (1 - (Real.sqrt ρ) - δ ) ((Real.sqrt ρ) / 20)
+    ((m - 1) * d^2) / ((Fintype.card F) * (2 * min_val)^7)
+)
