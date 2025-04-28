e0acdb5da047e76a0e7e809b59a230e0a083ae5f
PlanetMacro <mirco@leastauthority.com>
2025-04-28 13:43:32 +0200

    naming update

--- STIR.lean
+++ STIR.lean
@@ -1,9 +1,14 @@
 -- This module serves as the root of the `STIR` library.
 -- Import modules here that should be built as part of the library.
-import STIR.DefFracHammingDist
-import STIR.DefReedSolomonCodes
-import STIR.LemJohnsonBound
-import STIR.LemOutOfDomainSmpl
-import STIR.LemProximityGap
-import STIR.LemQuotienting
-import STIR.LemCombine
+
+import STIR.Combine
+import STIR.Folding
+import STIR.FracHammingDist
+import STIR.JohnsonBound
+import STIR.OutOfDomainSmpl
+import STIR.ProximityBound
+import STIR.ProximityGap
+import STIR.Quotienting
+import STIR.ReedSolomonCodes
+import STIR.SmoothDom
+import STIR.STIR_main
